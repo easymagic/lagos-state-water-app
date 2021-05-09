369b8def90636738989d05fac9fcd26ba3d22eb1
easymagic <nnamware@yahoo.com>
2021-05-09 22:34:11 +0100

    np

--- app/src/main/java/com/example/mylaswarcoapp/DashboardActivity.java
+++ app/src/main/java/com/example/mylaswarcoapp/DashboardActivity.java
@@ -3,12 +3,32 @@ package com.example.mylaswarcoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    TextView complaint = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        init();
+    }
+
+    void init(){
+
+        complaint = findViewById(R.id.clientserv);
+
+        complaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+
     }
 }
